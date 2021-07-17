a6d5ab5d5fa32238c7b4fa8cda67022e13a16219
missi0415 <missi0415@gmail.com>
2021-07-17 12:29:54 +0900

    @clickの使用

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        {{ item.text }}
-      </li>
-    </ul>
+    <button @click="clickEvent()">ボタンだよ</button>
   </div>
 </template>
 
   <script>
   export default {
     name: "HelloWorld",
-    data: () => ({
-      items: [
-        { id: 1, text: 'アイテム1' },
-        { id: 2, text: 'アイテム2' },
-        { id: 3, text: 'アイテム3' }
-      ],
-    }),
+    methods: {
+      clickEvent() {
+        console.log("ボタンが押されました");
+      },
+    },
   };
   </script>
