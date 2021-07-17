5c0a771a7f1a4df5a265116711515ec063fd59d2
missi0415 <missi0415@gmail.com>
2021-07-17 12:04:43 +0900

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="visibility">
+    <p>visibility: true</p>
+    <p>{{ message }}</p>
+  </div>
+  <div v-else>
+    <p>visivility: false</p>
+  </div>
+</template>
+
+  <script>
+  export default {
+    name: "HelloWorld",
+    data: () => ({
+      message: 'Hello World!',
+      visibility: true,
+    }),
+  };
+  </script>
