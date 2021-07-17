9bf6ac31562aec001600662fee01af9ad65cb6e4
missi0415 <missi0415@gmail.com>
2021-07-17 22:44:22 +0900

    vuetify calendar

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,22 +1,31 @@
-<template>
-  <div>
-    <v-sheet height="100vh">
-      <v-calendar></v-calendar>
-    </v-sheet>
-  </div>
-</template>
+  <template>
+    <div>
+      <v-sheet height="6vh" class="d-flex align-center">
+      <v-toolbar-title>2020年 10月</v-toolbar-title>
+      </v-sheet>
+      <v-sheet height="94vh">
+        <v-calendar
+          v-model="value"
+          :events="events"
+          @change="fetchEvents"
+        />
+      </v-sheet>
+    </div>
+  </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+  import { mapGetters, mapActions } from 'vuex';
 
-
-export default {
-  name: 'Calendar',
-  computed: {
-    ...mapGetters('events', ['events']),
-  },
-  methods: {
-    ...mapActions('events', ['fetchEvents']),
-  }
-};
+  export default {
+    name: 'Calendar',
+    data: () => ({
+      value: new Date('2020-10-01'), // 表示する月を指定
+    }),
+    computed: {
+      ...mapGetters('events', ['events']),
+    },
+    methods: {
+      ...mapActions('events', ['fetchEvents']),
+    }
+  };
 </script>
